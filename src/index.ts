8b949a1e1635434d9e7a83111f8bd7fce152081d
mrichte8 <max.richter@smail.th-koeln.de>
2020-06-29 18:20:17 +0200

    ci: test github actions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import "./index.scss";
 
-import crawl from "./crawl";
 import createState from "./state";
 import createView from "./view";
 
 const state = createState<CrawlResult>();
 
 createView(state);
-
-state.on("created", (currentTerm) => {});
-
-state.on("loading", () => {});
