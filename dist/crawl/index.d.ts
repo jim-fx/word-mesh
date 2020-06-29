b37ecdd811fc59a534fce7315299ccc28ba05bcf
jim-fx <jim-x@web.de>
2020-06-29 22:01:31 +0000

    Deploying to gh-pages from  @ f498aaf9a4f3b63f8bb8e440fc54dfbac93d7769 🚀

--- dist/crawl/index.d.ts
+++ dist/crawl/index.d.ts
@@ -8,7 +8,7 @@ interface CrawlResult {
         source: string;
         target: string;
         weight: number;
-    };
+    }[];
 }
 declare const _default: (term: string, log?: (s: string, m: number, c: number) => void) => Promise<CrawlResult>;
 export default _default;
