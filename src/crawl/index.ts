4d6056346ff355241a1df30a2587ca6af79b16bc
mrichte8 <max.richter@smail.th-koeln.de>
2020-06-29 23:59:33 +0200

    fix: escape regex

--- src/crawl/index.ts
+++ src/crawl/index.ts
@@ -1,6 +1,7 @@
 interface Node {
   id: string;
   depth: number;
+  dtr?: number;
 }
 
 interface Edge {
@@ -19,12 +20,17 @@ interface CrawlResult {
     source: string;
     target: string;
     weight: number;
-  };
+  }[];
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 }
 
 const cleanTerms = (term, terms: string[]) => {
-  const r1 = new RegExp(`^${term} vs `, "gm");
-  const r2 = new RegExp(`^${term} `, "gm");
+  const escaped = escapeRegExp(term);
+  const r1 = new RegExp(`^${escaped} vs `, "gm");
+  const r2 = new RegExp(`^${escaped} `, "gm");
   return terms.map((t) => t.replace(r1, "").replace(r2, ""));
 };
 
@@ -74,13 +80,13 @@ const EdgeStore = () => {
   const edges: { [key: string]: Edge } = {};
   return {
     get: () => JSON.parse(JSON.stringify(Object.values(edges))),
-    add: ({ id: _term1 }: Node, { id: _term2 }: Node) => {
-      const [term1, term2] = [_term1, _term2].sort();
-
+    add: ({ id: term1 }: Node, { id: term2 }: Node) => {
       const id = term1 + term2;
-
+      const id2 = term2 + term1;
       if (id in edges) {
         edges[id].weight += 1;
+      } else if (id2 in edges) {
+        edges[id2].weight += 1;
       } else {
         edges[id] = {
           source: term1,
@@ -115,7 +121,75 @@ const handleSingleNode = (nodeStore, edgeStore, rootNode): Promise<Node[]> =>
   });
 
 const BATCH_SIZE = 5;
-const MAX_AMOUNT = 100;
+const MAX_AMOUNT = 150;
+const FILTER_PERCENTAGE = 0.5;
+
+function walkGraph(nodes: Node[], edges: Edge[]) {
+  return nodes
+    .sort((a, b) => (a.depth > b.depth ? 1 : -1))
+    .map((n) => {
+      return edges
+        .filter((e) => e.target === n.id)
+        .map((e) => {
+          return {
+            parent: nodes.find((_n) => _n.id === e.source),
+            node: n,
+            edge: e,
+          };
+        });
+    })
+    .flat();
+}
+
+const finalFilter = (nodes: Node[], edges: Edge[]) => {
+  const maxWeight = edges.reduce((a, b) => (a > b.weight ? a : b.weight), 0);
+
+  let maxDistance = 0;
+  const rootNode = nodes.find((n) => n.depth === 0);
+  rootNode.dtr = 0;
+
+  console.log("filtering " + nodes.length + " nodes");
+
+  // Calculate the distance to the root node, while taking
+  // the weight of the single connections into account
+  walkGraph(nodes, edges).forEach(({ parent, node, edge }) => {
+    console.log(node.depth, parent, node, edge);
+    if (!parent) {
+      //RootNode
+      node.dtr = 0;
+    } else {
+      //We need to inverse the weight
+      const distanceToRoot =
+        (parent.dtr || 0) + node.depth + (maxWeight - edge.weight);
+
+      maxDistance = Math.max(maxDistance, distanceToRoot);
+      node.dtr = (node.dtr || 0) + distanceToRoot;
+    }
+  });
+
+  console.log("max distance to root " + maxDistance);
+
+  //Remove a certain percentage of nodes
+  const filteredNodes = nodes
+    .sort((a, b) => (a.dtr > b.dtr ? 1 : -1))
+    .slice(0, Math.floor(nodes.length * FILTER_PERCENTAGE));
+
+  const nodeIds = {};
+  filteredNodes.forEach((n) => {
+    nodeIds[n.id] = true;
+  });
+
+  console.log(
+    "filtered out " + (nodes.length - filteredNodes.length) + " nodes"
+  );
+
+  //Filter out all edges which connect to removed nodes;
+  const filteredEdges = edges.filter(
+    (e) => e.source in nodeIds && e.target in nodeIds
+  );
+
+  return { nodes: filteredNodes, edges: filteredEdges };
+};
 
 export default (
   term: string,
@@ -146,13 +220,16 @@ export default (
           if (queue.length && nodeStore.size() < MAX_AMOUNT) {
             workQueue();
           } else {
-            setTimeout(() => {
-              resolve({
-                term,
-                nodes: nodeStore.get(),
-                edges: edgeStore.get(),
-              });
-            }, 500);
+            const { nodes, edges } = finalFilter(
+              nodeStore.get(),
+              edgeStore.get()
+            );
+
+            resolve({
+              term,
+              nodes,
+              edges,
+            });
           }
         })
         .catch(reject);
